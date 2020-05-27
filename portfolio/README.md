8d876ecaaa641b4258288d3af161c28f5eb3e2da
Iris Liu <jiayiliu@live.unc.edu>
2020-05-27 13:03:15 -0400

    Delete me.html and photo.html, Update jquery and photo for project

--- portfolio/README.md
+++ portfolio/README.md
@@ -1,8 +1,20 @@
 This directory is where you'll write all of your code!
 
-By default it contains a barebones web app. To run a local server, execute this
-command:
+Library Requirement
+```bash
+npm init
+npm install bulma
+```
+To run a local server, execute this
+command in ~/step/portfolio:
 
 ```bash
-mvn package appengine:run
+ mvn package appengine:run
 ```
+and visit the webapp at http://localhost:8080, or
+
+```bash
+npm install browser-sync
+npx browser-sync start -sw
+```
+and visit the webapp at http://localhost:3000
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,17 +12,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ["This is Iris", "A random string", 'Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+$(document).ready(function(){
+    $('#workout-ninja-button').click(function(){
+        $('#project-pic').attr('src','images/Workout-Ninja.jpg');
+    });
+    $('#2048-button').click(function(){
+        $('#project-pic').attr('src','images/2048-image.png');
+    });
+    
+});
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+/* 
+export const render = function() {
+   $('#2048-button').click(function(){
+        $('#project-pic').attr('src','images/2048-image.png');
+    });
 }
+*/
