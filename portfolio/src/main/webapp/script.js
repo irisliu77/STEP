fd4fd38fbfbc9651d026df1ca4e77811889c6295
Iris Liu <jiayiliu@live.unc.edu>
2020-05-28 11:47:19 -0400

    remove project image column, change padding for hero body, remove burger button

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -11,13 +11,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-$(document).ready(function(){
-    $('#workout-ninja-button').click(function(){
-        $('#project-pic').attr('src','images/Workout-Ninja.jpg');
-    });
-    $('#2048-button').click(function(){
-        $('#project-pic').attr('src','images/2048-image.png');
-    });
-    
-});
