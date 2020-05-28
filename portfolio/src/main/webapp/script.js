e5073b922784740fdde2f1da2655b1a860ca863d
Iris Liu <jiayiliu@live.unc.edu>
2020-05-28 18:55:47 -0400

    dummy info page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -11,25 +11,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-$(document).ready(function(){
-    $('#workout-ninja-button').click(function(){
-        $('#project-pic').attr('src','images/Workout-Ninja.jpg');
-    });
-
-    $('#2048-button').click(function(){
-        $('#project-pic').attr('src','images/2048-image.png');
-    });
-
-    $('#tweet-button').click(function(){
-        $('#project-pic').attr('src','images/tweeter-image.png');
-    });
-
-    $('#mmr-button').click(function(){
-        $('#project-pic').attr('src','images/mmr-image.png');
-    });
-    
-    $('#job-button').click(function(){
-        $('#project-pic').attr('src','images/job-image.png');
-    });
-});
