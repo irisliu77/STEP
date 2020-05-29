1a501874af4c35cfdebd26791b423517b319bcfa
Iris Liu <jiayiliu@live.unc.edu>
2020-05-28 20:21:45 -0400

    format script.js

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,15 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-$(document).ready(function(){
+$(document).ready(function() {
     handleFilterButton();
 });
 
-const handleFilterButton = function(){
-    $('#photo-button').on('click',function(){
-        $('.photo').hide();
-        let input = $('#photo-input').val();
-        console.log(input);
-        $('.'+input).show();
+const handleFilterButton = function() {
+    $('#photo-button').on('click',function() {
+        $('.photo').css("display","none");
+        const input = $('#photo-input').val();
+        $('.' + input).css("display","inline");
     });   
 }
