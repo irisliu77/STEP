87e2e9a899b3b76ffaf21ad1ec56e9ae6fb70430
Iris Liu <jiayiliu@live.unc.edu>
2020-05-29 14:01:01 -0400

    add unfilter function

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,13 +13,26 @@
 // limitations under the License.
 
 $(document).ready(function() {
+    $('#unfilter-button').css("display","none");
     handleFilterButton();
+    handleUnfilterButton();
 });
 
 const handleFilterButton = function() {
-    $('#photo-button').on('click',function() {
+    $('#filter-button').on('click',function() {
         $('.photo').css("display","none");
+        $('#filter-button').css("display", "none");
+        $('#unfilter-button').css("display", "inline");
         const input = $('#photo-input').val();
         $('.' + input).css("display","inline");
     });   
+}
+
+const handleUnfilterButton = function() {
+   $('#unfilter-button').on('click', function() {
+        $('.photo').css("display", "inline");
+        $('#filter-button').css("display", "inline");
+        $('#unfilter-button').css("display", "none");
+        $('#photo-input').val('');
+   })
 }
