6eec3ec1e6cbc6dac6e99119e8f15d7723995f2c
Iris Liu <jiayiliu@live.unc.edu>
2020-06-05 01:50:22 +0000

    resolve conflict

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -38,56 +38,57 @@ public class DataServlet extends HttpServlet {
     private static final String content = "content";
     private static final String timestamp = "timestamp";
     private static final int defaultMaxComments = 10;
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    Gson gson = new Gson();
-    int limit = getRequestNum(request);
-    ArrayList<Comment> comments = getComments(limit);
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+      Gson gson = new Gson();
+      int limit = getRequestNum(request);
+      ArrayList<Comment> comments = getComments(limit);
 
-    response.setContentType("application/json;");
-    response.getWriter().println(gson.toJson(comments));
-  }
-
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-      String text = request.getParameter("comment-input");
-      long time = System.currentTimeMillis();
-
-      Entity comment = new Entity("Comment");
-      comment.setProperty(content, text);
-      comment.setProperty(timestamp,time);
-
-      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-      datastore.put(comment);
-      int max = getRequestNum(request);  
-      response.sendRedirect("/index.html?limit=" + max);
-  }
-
-  private int getRequestNum(HttpServletRequest request) {
-      String limitString = request.getParameter("limit");
-      try {
-          int limit = Integer.parseInt(limitString);
-      } catch(NumberFormatException e) {
-          return defaultMaxComments;
-      }
-      return limit;
-  } 
-
-  private ArrayList<Comment> getComments(int limit) {
-    Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    PreparedQuery res = datastore.prepare(query);
-
-    ArrayList<Comment> comments = new ArrayList<Comment>();
-    for(Entity entity : res.asIterable()) {
-        String content = (String) entity.getProperty("content");
-        long timestamp = (long) entity.getProperty("timestamp");
-        Comment comment = new Comment(content, timestamp);
-        comments.add(comment);
-        if(comments.size() > limit) {
-            break;
-        }
+      response.setContentType("application/json;");
+      response.getWriter().println(gson.toJson(comments));
     }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String text = request.getParameter("comment-input");
+        long time = System.currentTimeMillis();
+
+        Entity comment = new Entity("Comment");
+        comment.setProperty(content, text);
+        comment.setProperty(timestamp,time);
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.put(comment);
+        int max = getRequestNum(request);  
+        response.sendRedirect("/index.html?limit=" + max);
+    }
+
+    private int getRequestNum(HttpServletRequest request) {
+        String limitString = request.getParameter("limit");
+        int limit;
+        try {
+            limit = Integer.parseInt(limitString);
+        } catch(NumberFormatException e) {
+            return defaultMaxComments;
+        }
+        return limit;
+    } 
+
+    private ArrayList<Comment> getComments(int limit) {
+        Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        PreparedQuery res = datastore.prepare(query);
+
+        ArrayList<Comment> comments = new ArrayList<Comment>();
+        for(Entity entity : res.asIterable()) {
+            String content = (String) entity.getProperty("content");
+            long timestamp = (long) entity.getProperty("timestamp");
+            Comment comment = new Comment(content, timestamp);
+            comments.add(comment);
+            if(comments.size() > limit) {
+                break;
+            }
+        }
     return comments;
-  }
+    }
 }
